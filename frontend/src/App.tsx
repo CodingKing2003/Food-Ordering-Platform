82f85e6367326878178c1e9d13aa2787828dc9b8
Rajan <rajanj448@student.sfit.ac.in>
2024-03-12 10:35:19 +0530

    layout and header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
 
 
 import { Route, Routes } from 'react-router-dom'
+import Layout from './layouts/Layout'
+import HomePage from './pages/HomePage'
 
 function App() {
 
 
   return (
    <Routes>
-    <Route path='/' element={<h1>Home</h1>} />
+    <Route path='/' element={<Layout showHero>
+      <HomePage />
+
+    </Layout>} />
 
 
    </Routes>
